5182dd82ee4779fec458a149568663e687b4f48e
Shankar Weldmesh Pvt. Ltd <77322849+ManitKedia@users.noreply.github.com>
2021-09-23 14:49:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,24 @@
-function setup() 
-{
+function setup() {
   canvas = createCanvas(300, 300);
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
-  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded);
 }
 
-function draw()
-{
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function draw() {
   image(video, 0, 0, 300, 300);
-  classifier.classify(video, gotResult);  
+  classifier.classify(video, gotResult);
 }
 
-function modelLoaded()
-{
-  console.log('Model Loaded!');
-}
-
-function gotResult(error, results)
-{
+function gotResult(error, results) {
   if (error) {
     console.error(error);
-  }
-  else {
+  } else {
     console.log(results);
     document.getElementById("result_object_name").innerHTML = results[0].label;
     document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
